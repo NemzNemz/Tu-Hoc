0f90736b6e00b5e626ccbcf2895bc9f3782ef331
NemzNemz <urgot0933@gmail.com>
2025-11-09 21:22:51 +0700

    Add bt21 ptr-struct

--- Code-C/Ngay-09/Pointer21.c
+++ Code-C/Ngay-09/Pointer21.c
@@ -8,29 +8,32 @@ và in ra giá trị các trường qua con trỏ.
 typedef struct 
 {
     unsigned int id;
-    unsigned char name;
-    uint8_t score;
-}STU;/*
-Viết struct Student gồm id, name, score. 
-Khai báo một biến kiểu Student, tạo con trỏ trỏ đến nó, 
-và in ra giá trị các trường qua con trỏ.
-*/
-#include "stdio.h"
-#include "stdint.h"
-typedef struct 
-{
-    unsigned int id;
     unsigned char name[30];
     uint8_t score;
 }STUDENT;
+STUDENT student;
+//API
+void update_struct(STUDENT *std);
+void in_tt(STUDENT *std);
+//FUNCTION
+void update_struct(STUDENT *std){
+    printf("Moi ban nhap du lieu sinh vien!\n");
+    printf("ID: ");
+    scanf(" %d", &std->id);
+    printf("Ho va Ten: ");
+    scanf(" %20s", &std->name);
+    printf("Diem so: ");
+    scanf(" %d", &std->score);
+}
 
-STUDENT student ={
-    .id     = 0,
-    .name   = "0",
-    .score  = 0
-};
+void in_tt(STUDENT *std){
+    printf("ID Sinh Vien la %d \n", std->id);
+    printf("Ho Va Ten SV la %s \n", std->name);
+    printf("Diem sinh vien la %d \n", std->score);;
+}
 
 int main(void){
-
+    update_struct(&student);
+    in_tt(&student);
     return 0;
 }
