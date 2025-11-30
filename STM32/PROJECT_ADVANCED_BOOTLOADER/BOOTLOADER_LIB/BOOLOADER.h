955f0f073c0bb8bf530ccb158efe5a8303572e3d
NemzNemz <urgot0933@gmail.com>
2025-11-30 21:39:44 +0700

    Thêm project Advanced Bootloader – thư viện BOOTLOADER và main.c

---
+++ STM32/PROJECT_ADVANCED_BOOTLOADER/BOOTLOADER_LIB/BOOLOADER.h
@@ -0,0 +1,10 @@
+#ifndef __BOOTLOADER_H
+#define __BOOTLOADER_H
+#include "stm32f1xx_hal.h"
+
+void bootloader_erase(uint32_t address);
+void bootloader_write(uint32_t address, uint8_t *mData, uint16_t lenght);
+void bootloader_lock();
+void bootloader_unlock();
+void bootloader_read(uint32_t address, uint8_t *mData, uint16_t lenght);
+#endif
